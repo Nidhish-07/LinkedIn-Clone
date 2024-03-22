c7232e712e22d2b89057842b45ba101149bded08
Nidhish Chauhan <rch246chauhan@gmail.com>
2024-03-22 20:25:10 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignIn from './components/SignIn'
+
+const App = () => {
+  return (
+    <div>
+      <SignIn></SignIn>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,15 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>
+        <Grid container>
+            <Grid item xs={6}>Authentication</Grid>
+            <Grid item xs={6}>Developer</Grid>
+        </Grid>
+    </div>
+  )
+}
+
+export default SignIn
